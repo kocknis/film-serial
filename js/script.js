116e0d923412160c649d50242d9655cc85de25b3
Sina <bekarsina@gmail.com>
2023-04-14 19:03:58 +0430

    dynamic dropdown on header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-const $ = document
+const $ = document;
 
 // updata of data with today data
-const todayData = $.querySelector('.today-data')
-todayData.innerHTML = new Date().toLocaleDateString('fa-IR')
+const todayData = $.querySelector(".today-data");
+todayData.innerHTML = new Date().toLocaleDateString("fa-IR");
+
+
+// dynamic dropdown on header 
+const dropdownNavbarButton = $.querySelector(".dropdown-nav"),
+      dropdownGroupList = $.querySelector(".dropdown-group");
+
+dropdownNavbarButton.addEventListener("click", (event) => {
+    event.preventDefault()
+  if (dropdownGroupList.style.visibility === "hidden") {
+    dropdownGroupList.style.visibility = "visible";
+  } else {
+    dropdownGroupList.style.visibility = "hidden";
+  }
+});
