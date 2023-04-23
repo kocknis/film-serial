66b452a5ffc3c571d550f588318ca4831c7a7f56
Sina <bekarsina@gmail.com>
2023-04-23 22:10:19 +0330

    finished the ptoject

--- js/register.js
+++ js/register.js
@@ -28,9 +28,6 @@ signupFormBtn.addEventListener("click", (event) => {
         userPhone: userPhoneNumberEmailForSingupValue,
       };
 
-      console.log(oldUsers);
-
-
       localStorage.setItem("users", JSON.stringify(oldUsers));
       window.location.href = "http://127.0.0.1:5500/index.html";
 
